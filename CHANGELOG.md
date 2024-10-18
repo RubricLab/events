481cb7fbadea6071682512fdaacba46be4283f05
DexterStorey <dexter@dexterstorey.com>
2024-10-18 01:19:14 -0400

    migrate event to functions vs. class

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
+- [2024-10-18] [migrate event to functions vs. class](https://github.com/RubricLab/events/commit/5be674bb0013563b2c897742e676d6a059318dec)
 - [2024-10-17] [Connect send and publish methods](https://github.com/RubricLab/events/commit/417145ada1f17d31be2a5f4f962badb662be601f)
 - [2024-10-17] [Add useEvent hook](https://github.com/RubricLab/events/commit/d73ace219e6efd841ae811c91fc1543e32c5e2f9)
 - [2024-10-17] [demo-commit](https://github.com/RubricLab/events/commit/816cea8d14674a7120242d9ea29b5611dad28be9)
--- events/index.ts
+++ events/index.ts
@@ -1,79 +1,165 @@
 import Redis from 'ioredis'
 import type { GenericEventType } from '../types'
 
-export class EventDispatcher<EventType extends GenericEventType> {
-	subscriber: Redis
-	publisher: Redis
+// export class EventDispatcher<EventType extends GenericEventType> {
+// 	subscriber: Redis
+// 	publisher: Redis
 
-	constructor({ redisURL }: { redisURL: string }) {
-		this.subscriber = new Redis(redisURL)
-		this.publisher = new Redis(redisURL)
-	}
+// 	constructor({ redisURL }: { redisURL: string }) {
+// 		this.subscriber = new Redis(redisURL)
+// 		this.publisher = new Redis(redisURL)
+// 	}
 
-	async send<EventInstance extends Exclude<keyof EventType, number | symbol>>(
-		channel: EventInstance,
-		event: EventType[EventInstance]
-	) {
-		console.log('Not implemented')
-		console.log(channel, event)
+// 	async send<EventInstance extends Exclude<keyof EventType, number | symbol>>(
+// 		channel: EventInstance,
+// 		event: EventType[EventInstance]
+// 	) {
+// 		console.log('Not implemented')
+// 		console.log(channel, event)
 
-		await fetch('/api/core', {
-			method: 'POST',
-			body: JSON.stringify({
-				message: JSON.stringify(event) // TODO: match event type to target
-			})
-		})
+// 		// await fetch('/api/core', {
+// 		// 	method: 'POST',
+// 		// 	body: JSON.stringify({
+// 		// 		message: JSON.stringify(event) // TODO: match event type to target
+// 		// 	})
+// 		// })
 
-		// await eventHandlers[action]?.(data, username)
-	}
+// 		// await eventHandlers[action]?.(data, username)
+// 	}
 
-	async publish<EventInstance extends Exclude<keyof EventType, number | symbol>>(
-		channel: string,
-		event: EventType[EventInstance]
-	) {
-		this.publisher.publish(channel, JSON.stringify(event))
-	}
+// 	async publish<EventInstance extends Exclude<keyof EventType, number | symbol>>(
+// 		channel: string,
+// 		event: EventType[EventInstance]
+// 	) {
+// 		this.publisher.publish(channel, JSON.stringify(event))
+// 	}
 
-	subscribe<EventInstance extends Exclude<keyof EventType, number | symbol>>(
-		channel: EventInstance,
-		_callback: (event: EventType[EventInstance]) => void
-	) {
-		console.log('Not implemented')
-		console.log(channel)
-	}
+// 	subscribe<EventInstance extends Exclude<keyof EventType, number | symbol>>(
+// 		channel: EventInstance,
+// 		_callback: (event: EventType[EventInstance]) => void
+// 	) {
+// 		console.log('Not implemented')
+// 		console.log(channel)
+// 	}
 
-	async handler(req: Request): Promise<Response> {
-		const { searchParams } = new URL(req.url)
-		const id = searchParams.get('id')
+// 	async handler(req: Request): Promise<Response> {
+// 		const { searchParams } = new URL(req.url)
+// 		const id = searchParams.get('id')
 
-		if (!id) return new Response('No id provided', { status: 400 })
+// 		if (!id) return new Response('No id provided', { status: 400 })
 
-		const { writable, readable } = new TransformStream()
-		const writer = writable.getWriter()
+// 		const { writable, readable } = new TransformStream()
+// 		const writer = writable.getWriter()
 
-		const encoder = new TextEncoder()
+// 		const encoder = new TextEncoder()
 
-		const listener = async (channel: string, message: string) => {
-			if (channel === id) {
-				await writer.write(encoder.encode(`data: ${message}\n\n`))
-			}
+// 		const listener = async (channel: string, message: string) => {
+// 			if (channel === id) {
+// 				await writer.write(encoder.encode(`data: ${message}\n\n`))
+// 			}
+// 		}
+
+// 		console.log('YOO')
+
+// 		console.log(this.subscriber)
+
+// 		this.subscriber.subscribe(id)
+// 		this.subscriber.on('message', listener)
+
+// 		req.signal.addEventListener('abort', () => {
+// 			this.subscriber.unsubscribe(id)
+// 			this.subscriber.off('message', listener)
+// 			writer.close()
+// 		})
+
+// 		return new Response(readable, {
+// 			headers: {
+// 				'Content-Type': 'text/event-stream',
+// 				'Cache-Control': 'no-cache',
+// 				Connection: 'keep-alive'
+// 			}
+// 		})
+// 	}
+// }
+
+export function createEventActions<EventType extends GenericEventType>({
+	redisURL
+}: { redisURL: string }) {
+	const publisher = new Redis(redisURL)
+
+	return {
+		async send<EventInstance extends Exclude<keyof EventType, number | symbol>>(
+			channel: EventInstance,
+			event: EventType[EventInstance]
+		) {
+			console.log('Not implemented')
+			console.log(channel, event)
+
+			// await fetch('/api/core', {
+			// 	method: 'POST',
+			// 	body: JSON.stringify({
+			// 		message: JSON.stringify(event) // TODO: match event type to target
+			// 	})
+			// })
+
+			// await eventHandlers[action]?.(data, username)
+		},
+
+		async publish<EventInstance extends Exclude<keyof EventType, number | symbol>>(
+			channel: string,
+			event: EventType[EventInstance]
+		) {
+			console.log('publishing event to ', channel)
+			publisher.publish(channel, JSON.stringify(event))
+		},
+
+		subscribe<EventInstance extends Exclude<keyof EventType, number | symbol>>(
+			channel: EventInstance,
+			_callback: (event: EventType[EventInstance]) => void
+		) {
+			console.log('Not implemented')
+			console.log(channel)
 		}
+	}
+}
 
-		this.subscriber.subscribe(id)
-		this.subscriber.on('message', listener)
+export function createEventsHandler({ redisURL }: { redisURL: string }) {
+	const subscriber = new Redis(redisURL)
+	return {
+		async eventsHandler(req: Request): Promise<Response> {
+			const { searchParams } = new URL(req.url)
+			const id = searchParams.get('id')
 
-		req.signal.addEventListener('abort', () => {
-			this.subscriber.unsubscribe(id)
-			this.subscriber.off('message', listener)
-			writer.close()
-		})
+			if (!id) return new Response('No id provided', { status: 400 })
 
-		return new Response(readable, {
-			headers: {
-				'Content-Type': 'text/event-stream',
-				'Cache-Control': 'no-cache',
-				Connection: 'keep-alive'
+			const { writable, readable } = new TransformStream()
+			const writer = writable.getWriter()
+
+			const encoder = new TextEncoder()
+
+			const listener = async (channel: string, message: string) => {
+				console.log('received message', message, ' to ', channel)
+				if (channel === id) {
+					await writer.write(encoder.encode(`data: ${message}\n\n`))
+				}
 			}
-		})
+
+			subscriber.subscribe(id)
+			subscriber.on('message', listener)
+
+			req.signal.addEventListener('abort', () => {
+				subscriber.unsubscribe(id)
+				subscriber.off('message', listener)
+				writer.close()
+			})
+
+			return new Response(readable, {
+				headers: {
+					'Content-Type': 'text/event-stream',
+					'Cache-Control': 'no-cache',
+					Connection: 'keep-alive'
+				}
+			})
+		}
 	}
 }
